61cc34fa3cd08b5ad57501804fa921b58bdf7671
lsj0822 <lsj0822@khu.ac.kr>
2024-01-21 22:21:14 +0900

    5430수정

--- C++/07_덱/0x07_[G5]5430.cpp
+++ C++/07_덱/0x07_[G5]5430.cpp
@@ -1,65 +1,65 @@
-//#include <bits/stdc++.h>
-//
-//using namespace std;
-//int T, n;
-//string p, x;
-//deque<int> DQ;
-//int main() {
-//	cin >> T;
-//	while (T--) {
-//		DQ.clear();
-//		cin >> p >> n >> x;
-//		bool reverse = false, flag = true;
-//		int addn = 0;
-//		for (int i = 1; i < x.length(); i++) {
-//			if (x[i] == ',' or x[i] == ']') {
-//				if (addn) {
-//					DQ.push_back(addn);
-//				}
-//				addn = 0;
-//			}
-//			else {
-//				addn = addn * 10 + static_cast<int>(x[i] - '0');
-//			}
-//		}
-//		for (auto action : p) {
-//			if (action == 'D') {
-//				if (DQ.empty()) {
-//					flag = false;
-//					break;
-//				}
-//				else {
-//					if (reverse) DQ.pop_back();
-//					else DQ.pop_front();
-//				}
-//			}
-//			else if (action == 'R') {
-//				reverse = !reverse;
-//			}
-//		}
-//
-//		if (flag) {
-//			if (DQ.empty()) {
-//				cout << "[]" << '\n';
-//			}
-//			else {
-//				cout << '[';
-//				while (DQ.size() != 1) {
-//					if (reverse) {
-//						cout << DQ.back() << ",";
-//						DQ.pop_back();
-//					}
-//					else {
-//						cout << DQ.front() << ",";
-//						DQ.pop_front();
-//					}
-//				}
-//				cout << DQ.front() << ']' << '\n';
-//			}
-//		}
-//		else {
-//			cout << "error" << '\n';
-//		}
-//
-//	}
-//}
+#include <bits/stdc++.h>
+
+using namespace std;
+int T, n;
+string p, x;
+deque<int> DQ;
+int main() {
+	cin >> T;
+	while (T--) {
+		DQ.clear();
+		cin >> p >> n >> x;
+		bool reverse = false, flag = true;
+		int addn = 0;
+		for (int i = 1; i < x.length(); i++) {
+			if (x[i] == ',' or x[i] == ']') {
+				if (addn) {
+					DQ.push_back(addn);
+				}
+				addn = 0;
+			}
+			else {
+				addn = addn * 10 + static_cast<int>(x[i] - '0');
+			}
+		}
+		for (auto action : p) {
+			if (action == 'D') {
+				if (DQ.empty()) {
+					flag = false;
+					break;
+				}
+				else {
+					if (reverse) DQ.pop_back();
+					else DQ.pop_front();
+				}
+			}
+			else if (action == 'R') {
+				reverse = !reverse;
+			}
+		}
+
+		if (flag) {
+			if (DQ.empty()) {
+				cout << "[]" << '\n';
+			}
+			else {
+				cout << '[';
+				while (DQ.size() != 1) {
+					if (reverse) {
+						cout << DQ.back() << ",";
+						DQ.pop_back();
+					}
+					else {
+						cout << DQ.front() << ",";
+						DQ.pop_front();
+					}
+				}
+				cout << DQ.front() << ']' << '\n';
+			}
+		}
+		else {
+			cout << "error" << '\n';
+		}
+
+	}
+}
