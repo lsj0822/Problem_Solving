7650095516618ab346877aa3c4a9e0e35a24eb59
lsj0822 <lsj0822@khu.ac.kr>
2024-01-31 00:59:30 +0900

    13시뮬레이션_0130

---
+++ C++/13_시뮬레이션/0x0d_[G3]18808.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<algorithm>
+#define X first
+#define Y second
+
+using namespace std;
+
+int notebook[41][41];
+int n, m, k, ans = 0;
+
+void sticker_rotate(int container[][11], int* r, int* c) {
+	int new_container[11][11];
+	for (int i = 0; i < *r; i++) {
+		for (int j = 0; j < *c; j++) {
+			new_container[j][*r-i-1] = container[i][j];
+		}
+	}
+	copy(&new_container[0][0], &new_container[10][10], &container[0][0]);
+	int temp = *r;
+	*r = *c;
+	*c = temp;
+}
+bool detect_sticker(int container[][11], int x, int y, int r, int c) {
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (notebook[x + i][y + j] && container[i][j]) return false;
+		}
+	}
+	return true;
+}
+
+bool input_sticker(int container[][11], int r, int c) {
+	for (int i = 0; i <= n - r; i++) {
+		for (int j = 0; j <= m - c; j++) {
+				if (detect_sticker(container, i, j, r, c)) {
+					for (int x = 0; x < r; x++) {
+						for (int y = 0; y < c; y++) {
+							if(container[x][y])	notebook[i + x][j + y] = container[x][y];
+						}
+					}
+					return true;
+				}
+		}
+	}
+	return false;
+}
+int main() {
+	cin >> n >> m >> k;
+	while (k--) {
+		int r, c;
+		cin >> r >> c;
+		int sticker[11][11];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				cin >> sticker[i][j];
+			}
+		}
+		int rot = 4;
+		while (rot--) {
+			if (input_sticker(sticker, r, c)) break;
+			sticker_rotate(sticker, &r, &c);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			ans += notebook[i][j];
+		}
+	}
+	cout << ans;
+}
